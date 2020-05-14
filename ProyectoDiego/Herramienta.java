5b57a3d1408d0b13f717312ddf005ae116c35f5e
ErnoMitrovic <55798237+ErnoMitrovic@users.noreply.github.com>
2020-05-14 15:56:44 -0500

    Update Herramienta.java

--- ProyectoDiego/Herramienta.java
+++ ProyectoDiego/Herramienta.java
@@ -1,3 +1,43 @@
-public class Herramienta{
+import java.util.Random;
+import java.util.Scanner;
 
+public class Herramienta{
+  Random rand = new Random();
+  Scanner scan = new Scanner(System.in);
+  
+  public String nombreDeHerramienta;
+  public final ID = rand.ints(0,1000);
+  protected int cantidad;
+  private boolean disponible;
+  public Herramienta(){
+    nombreDeHerramienta = "";
+    cantidad = 0;
+    disponible = false;
+  }
+
+  public void registrarHerramienta(){
+    System.out.println("Pon el nombre de la herramienta\n");
+    nombreDeHerramienta = scan.next();
+    System.out.println("Ingresa la cantidad de " + nombreDeHerramienta + " que registras\n");
+    cantidad += scan.nextInt();
+    if (cantidad > 0){
+      disponible = true;
+    }
+  }
+
+  public void prestarHerramienta(int cantidad){
+    if (this.cantidad - cantidad >= 0){
+      System.out.println("Toma "+ cantidad + " " + nombreDeHerramienta);
+      this.cantidad -= cantidad;
+    }
+    else{
+      System.out.println("No tenemos "+ cantidad + " " + nombreDeHerramienta);
+    }
+    if (this.cantidad == 0) disponible = false
+  }
+
+  public void devolverHerramienta(int cantidad){
+    this.cantidad += cantidad;
+    System.out.println("Devolviste " + cantidad + " "+ nombreDeHerramienta);
+  }
 }
