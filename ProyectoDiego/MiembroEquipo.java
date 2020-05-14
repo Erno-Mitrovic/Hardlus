a6c0c545442e6024cd39510324c09b8ed56df5ce
ErnoMitrovic <55798237+ErnoMitrovic@users.noreply.github.com>
2020-05-14 16:11:57 -0500

    Update MiembroEquipo.java

--- ProyectoDiego/MiembroEquipo.java
+++ ProyectoDiego/MiembroEquipo.java
@@ -1,1 +1,9 @@
-
+public class MiembroEquipo{
+  public String miembroEquipo;
+  public MiembroEquipo(){
+    this.miembroEquipo = "";
+  }
+  public void registrarMiembro(String miembroEquipo){
+    this.miembroEquipo = miembroEquipo;
+  }
+}
